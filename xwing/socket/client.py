c11fc697afc16df1801f5f1dbd973fa5fe9e722e
Victor Poluceno <victorpoluceno@gmail.com>
2016-06-01 15:26:54 -0300

    Make Client.connect awaitable

--- xwing/socket/client.py
+++ xwing/socket/client.py
@@ -33,6 +33,7 @@ class Client(object):
         self.multiplex_endpoint = multiplex_endpoint
         self.identity = str(uuid.uuid1()) if not identity else identity
 
-    def connect(self, service):
+    async def connect(self, service):
         address, port = self.multiplex_endpoint.split(':')
-        return Connection(self.loop, connect((address, int(port)), service))
+        return Connection(self.loop, await connect(
+                self.loop, (address, int(port)), service))
