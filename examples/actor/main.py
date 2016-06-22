4040da1a9f0b6ef0b188ce02c89dfe421d34c123
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2016-06-21 22:58:12 -0300

    Add an actor implementation as example

--- examples/actor/main.py
+++ examples/actor/main.py
@@ -2,7 +2,7 @@ import asyncio
 import sys
 sys.path.append('.')  # NOQA
 
-from examples.actor.model import Node
+from examples.actor import Node
 
 
 async def pong_actor(mailbox):
@@ -19,7 +19,6 @@ if __name__ == '__main__':
     # python examples/actor/server.py
     hub_frontend, hub_backend = '127.0.0.1:5555', '/var/tmp/xwing.socket'
     loop = asyncio.get_event_loop()
-    loop.set_debug(1)
 
     # Start a actor node by which we can spawn actors
     node = Node(loop, hub_frontend, hub_backend)
@@ -27,6 +26,6 @@ if __name__ == '__main__':
     # Spawn an pong actor and get its id
     pong_id = node.spawn(pong_actor)
     node.spawn(ping_actor, pong_id)
+    node.run()
 
-    loop.run_until_complete(node.run())
     loop.close()
